a556ecd729d12955a086628d3fad2f13dea8dc0f
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-06-02 20:38:41 +0200

    Add project files.

---
+++ bookshop-dotnet-core-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bookshop_dotnet_core_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
