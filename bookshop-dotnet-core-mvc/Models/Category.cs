465d9e928d1bfde264596d6b4b2345e56ad2ea9c
milan-varnamo <milan.varnamo.sv.com>
2025-06-03 22:02:15 +0200

    Add Category model

---
+++ bookshop-dotnet-core-mvc/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace bookshop_dotnet_core_mvc.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+    }
+}
