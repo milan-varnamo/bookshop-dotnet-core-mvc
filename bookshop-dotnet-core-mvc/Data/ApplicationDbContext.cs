3d21a474852e67023965c0f7e809d254a26588f0
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-06-04 20:25:51 +0200

    Add ApplicationDbContext and SQL Server configuration

---
+++ bookshop-dotnet-core-mvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace bookshop_dotnet_core_mvc.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
