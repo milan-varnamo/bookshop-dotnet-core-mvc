9508f84c4d7f5a62c6dbb8c13ce02074c27dcf04
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-06-08 22:54:24 +0200

    Add display all categories in view functionality

--- bookshop-dotnet-core-mvc/Controllers/CategoryController.cs
+++ bookshop-dotnet-core-mvc/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using bookshop_dotnet_core_mvc.Data;
+using bookshop_dotnet_core_mvc.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace bookshop_dotnet_core_mvc.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> objCategoryList = _db.Categories.ToList();
+            return View(objCategoryList);
         }
     }
 }
