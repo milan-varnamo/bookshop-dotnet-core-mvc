7e78ed2a09be16643f4f6889c2997be577f9e8ff
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-06-08 22:43:20 +0200

    Fix minor formatting issue for uniformity

--- bookshop-dotnet-core-mvc/Data/ApplicationDbContext.cs
+++ bookshop-dotnet-core-mvc/Data/ApplicationDbContext.cs
@@ -15,7 +15,7 @@ namespace bookshop_dotnet_core_mvc.Data
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Category>().HasData(
-                new Category { Id=1, Name="Action", DisplayOrder=1},
+                new Category { Id = 1, Name = "Action", DisplayOrder = 1},
                 new Category { Id = 2, Name = "SciFi", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "History", DisplayOrder = 3 }
                 );
