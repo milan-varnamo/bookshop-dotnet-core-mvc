f6882fda2f1941148da506b9bd1d702a28c91200
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-06-11 22:32:33 +0200

    Implement input tag helpers

--- bookshop-dotnet-core-mvc/Models/Category.cs
+++ bookshop-dotnet-core-mvc/Models/Category.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace bookshop_dotnet_core_mvc.Models
 {
@@ -6,7 +7,9 @@ namespace bookshop_dotnet_core_mvc.Models
     {
         public int Id { get; set; }
         [Required]
+        [DisplayName("Category Name")]
         public string Name { get; set; }
-        public int DisplayOrder { get; set; }
+		[DisplayName("Display Order")]
+		public int DisplayOrder { get; set; }
     }
 }
