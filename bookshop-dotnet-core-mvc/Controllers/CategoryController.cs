3eb75f779791563ebbc906ddaa73b42a36542bf7
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-06-16 19:39:35 +0200

    Add TempData notifications for category index view

--- bookshop-dotnet-core-mvc/Controllers/CategoryController.cs
+++ bookshop-dotnet-core-mvc/Controllers/CategoryController.cs
@@ -33,6 +33,7 @@ namespace bookshop_dotnet_core_mvc.Controllers
             {
 				_db.Categories.Add(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category created successfully";
 				return RedirectToAction("Index");
 			}
             return View();
@@ -60,6 +61,7 @@ namespace bookshop_dotnet_core_mvc.Controllers
 			{
 				_db.Categories.Update(obj);
 				_db.SaveChanges();
+				TempData["success"] = "Category updated successfully";
 				return RedirectToAction("Index");
 			}
 			return View();
@@ -93,6 +95,7 @@ namespace bookshop_dotnet_core_mvc.Controllers
 
 			_db.Categories.Remove(obj);
 			_db.SaveChanges();
+			TempData["success"] = "Category deleted successfully";
 			return RedirectToAction("Index");
 		}
 	}
