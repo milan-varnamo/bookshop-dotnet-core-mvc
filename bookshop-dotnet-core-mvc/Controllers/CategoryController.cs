7a18e7efdcf85e3072e3252f13b9757809ed677d
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-06-22 21:20:39 +0200

    Replace DbContext with CategoryRepository

--- bookshop-dotnet-core-mvc/Controllers/CategoryController.cs
+++ bookshop-dotnet-core-mvc/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
 ﻿using Bookshop.DataAccess.Data;
+using Bookshop.DataAccess.Repository.IRepository;
 using Bookshop.Models;
 using Microsoft.AspNetCore.Mvc;
 
@@ -6,14 +7,14 @@ namespace bookshop_dotnet_core_mvc.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db;
-        public CategoryController(ApplicationDbContext db)
+        private readonly ICategoryRepository _categoryRepo;
+        public CategoryController(ICategoryRepository db)
         {
-            _db = db;
+            _categoryRepo = db;
         }
         public IActionResult Index()
         {
-            List<Category> objCategoryList = _db.Categories.ToList();
+            List<Category> objCategoryList = _categoryRepo.GetAll().ToList();
             return View(objCategoryList);
         }
 
@@ -31,8 +32,8 @@ namespace bookshop_dotnet_core_mvc.Controllers
 
             if (ModelState.IsValid)
             {
-				_db.Categories.Add(obj);
-				_db.SaveChanges();
+				_categoryRepo.Add(obj);
+				_categoryRepo.Save();
 				TempData["success"] = "Category created successfully";
 				return RedirectToAction("Index");
 			}
@@ -45,7 +46,7 @@ namespace bookshop_dotnet_core_mvc.Controllers
 				return NotFound();
 			}
 
-			Category? categoryFromDb = _db.Categories.Find(id);
+			Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id);
 
 			if (categoryFromDb == null)
 			{
@@ -59,8 +60,8 @@ namespace bookshop_dotnet_core_mvc.Controllers
 		{
 			if (ModelState.IsValid)
 			{
-				_db.Categories.Update(obj);
-				_db.SaveChanges();
+				_categoryRepo.Update(obj);
+				_categoryRepo.Save();
 				TempData["success"] = "Category updated successfully";
 				return RedirectToAction("Index");
 			}
@@ -74,7 +75,7 @@ namespace bookshop_dotnet_core_mvc.Controllers
 				return NotFound();
 			}
 
-			Category? categoryFromDb = _db.Categories.Find(id);
+			Category? categoryFromDb = _categoryRepo.Get(u => u.Id == id);
 
 			if (categoryFromDb == null)
 			{
@@ -86,15 +87,15 @@ namespace bookshop_dotnet_core_mvc.Controllers
 		[HttpPost, ActionName("Delete")]
 		public IActionResult DeletePOST(int? id)
 		{
-			Category? obj = _db.Categories.Find(id);
+			Category? obj = _categoryRepo.Get(u => u.Id == id);
 
 			if (obj == null)
 			{
 				return NotFound();
 			}
 
-			_db.Categories.Remove(obj);
-			_db.SaveChanges();
+			_categoryRepo.Remove(obj);
+			_categoryRepo.Save();
 			TempData["success"] = "Category deleted successfully";
 			return RedirectToAction("Index");
 		}
--- bookshop-dotnet-core-mvc/Program.cs
+++ bookshop-dotnet-core-mvc/Program.cs
@@ -1,4 +1,6 @@
 using Bookshop.DataAccess.Data;
+using Bookshop.DataAccess.Repository;
+using Bookshop.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -6,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
 
 var app = builder.Build();
 
