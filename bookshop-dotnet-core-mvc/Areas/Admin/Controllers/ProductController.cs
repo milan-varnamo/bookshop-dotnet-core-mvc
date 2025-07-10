598a9488c68e71491d5ee37fa2575a582f6ea81d
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-07-10 17:09:29 +0200

    Refactor create and edit product forms into a single upsert form

--- bookshop-dotnet-core-mvc/Areas/Admin/Controllers/ProductController.cs
+++ bookshop-dotnet-core-mvc/Areas/Admin/Controllers/ProductController.cs
@@ -23,7 +23,7 @@ namespace Bookshop.Areas.Admin.Controllers
             return View(objProductList);
         }
 
-        public IActionResult Create()
+        public IActionResult Upsert(int? id)
         {
 			IEnumerable<SelectListItem> CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
 			{
@@ -37,10 +37,21 @@ namespace Bookshop.Areas.Admin.Controllers
 				Product = new Product()
 			};
 
-			return View(productVM);
+			if (id == null || id == 0)
+			{
+				// Create
+				return View(productVM);
+			}
+			else
+			{
+				// Updatge
+				productVM.Product = _unitOfWork.Product.Get(u => u.Id == id);
+				return View(productVM);
+			}
+			
         }
         [HttpPost]
-		public IActionResult Create(ProductVM productVM)
+		public IActionResult Upsert(ProductVM productVM, IFormFile? file)
 		{
             if (ModelState.IsValid)
             {
@@ -60,35 +71,6 @@ namespace Bookshop.Areas.Admin.Controllers
 			}
 				
 		}
-		public IActionResult Edit(int? id)
-		{	
-			if (id == null || id == 0)
-			{
-				return NotFound();
-			}
-
-			Product? productFromDb = _unitOfWork.Product.Get(u => u.Id == id);
-
-			if (productFromDb == null)
-			{
-				return NotFound();
-			}
-
-			return View(productFromDb);
-		}
-		[HttpPost]
-		public IActionResult Edit(Product obj)
-		{
-			if (ModelState.IsValid)
-			{
-				_unitOfWork.Product.Update(obj);
-				_unitOfWork.Save();
-				TempData["success"] = "Product updated successfully";
-				return RedirectToAction("Index");
-			}
-			return View();
-		}
-
 		public IActionResult Delete(int? id)
 		{
 			if (id == null || id == 0)
