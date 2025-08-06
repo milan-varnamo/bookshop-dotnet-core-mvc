b0adee40533f46ac035c8dcf7e70b168e27161c5
milan-varnamo <milan.mlinar.sv@gmail.com>
2025-08-06 22:02:26 +0200

    Add roles to database

--- bookshop-dotnet-core-mvc/Program.cs
+++ bookshop-dotnet-core-mvc/Program.cs
@@ -3,6 +3,8 @@ using Bookshop.DataAccess.Repository;
 using Bookshop.DataAccess.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Bookshop.Utility;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,8 +12,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddRazorPages();
-builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
